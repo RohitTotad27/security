cb78ef6e58ce72d6757f4f6e465eec08e4704123
rohit <rohit@vedminds.com>
2022-08-16 18:36:42 +0530

    Security basics

---
+++ src/main/java/com/security/springsecurity/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package com.security.springsecurity;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //Set your configuration to auth object
+    }
+}
---
+++ src/main/java/com/security/springsecurity/controller/HomeResource.java
@@ -0,0 +1,15 @@
+package com.security.springsecurity.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+
+    @RequestMapping("/home")
+    public String message(){
+        return ("<h1>WELCOME</h1>");
+    }
+}
